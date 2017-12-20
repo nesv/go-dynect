203ac2792b7be091976cdd7d5750e198445e3a74
S. Brent Faulkner <brent.faulkner@shopify.com>
2017-12-20 06:47:57 -0500

    add tests for ConvenientClient and use fixtures

--- dynect/client_test.go
+++ dynect/client_test.go
@@ -1,9 +1,12 @@
 package dynect
 
 import (
+	"log"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/dnaeon/go-vcr/recorder"
 )
 
 var (
@@ -18,6 +21,46 @@ func init() {
 	DynUsername = os.Getenv("DYNECT_USER_NAME")
 	DynPassword = os.Getenv("DYNECT_PASSWORD")
 	testZone = os.Getenv("DYNECT_TEST_ZONE")
+}
+
+// test helper to begin recording or playback of vcr cassette
+func withCassette(cassetteName string, f func(*recorder.Recorder)) {
+	r, err := recorder.New(cassetteName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer r.Stop()
+
+	f(r)
+}
+
+// test helper to setup client with vcr cassette
+func withClient(cassetteName string, f func(*Client)) {
+	withCassette(cassetteName, func(r *recorder.Recorder) {
+		c := NewClient(DynCustomerName)
+		c.SetTransport(r)
+		c.Verbose(true)
+
+		f(c)
+	})
+}
+
+// test helper to setup authenticated client with vcr cassette
+func testWithClientSession(cassetteName string, t *testing.T, f func(*Client)) {
+	withClient(cassetteName, func(c *Client) {
+		if err := c.Login(DynUsername, DynPassword); err != nil {
+			t.Fatal(err)
+		}
+
+		defer func() {
+			if err := c.Logout(); err != nil {
+				t.Error(err)
+			}
+		}()
+
+		f(c)
+	})
 }
 
 func TestSetup(t *testing.T) {
@@ -39,76 +82,53 @@ func TestSetup(t *testing.T) {
 }
 
 func TestLoginLogout(t *testing.T) {
-	client := NewClient(DynCustomerName)
-	client.Verbose(true)
-	err := client.Login(DynUsername, DynPassword)
-	if err != nil {
-		t.Error(err)
-	}
+	withClient("fixtures/login_logout", func(c *Client) {
+		if err := c.Login(DynUsername, DynPassword); err != nil {
+			t.Error(err)
+		}
 
-	err = client.Logout()
-	if err != nil {
-		t.Error(err)
-	}
+		if err := c.Logout(); err != nil {
+			t.Error(err)
+		}
+	})
 }
 
 func TestZonesRequest(t *testing.T) {
-	client := NewClient(DynCustomerName)
-	client.Verbose(true)
-	err := client.Login(DynUsername, DynPassword)
-	if err != nil {
-		t.Error(err)
-	}
-	defer func() {
-		err = client.Logout()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
+	testWithClientSession("fixtures/zones_request", t, func(c *Client) {
+		var resp ZonesResponse
 
-	var resp ZonesResponse
-	err = client.Do("GET", "Zone", nil, &resp)
-	if err != nil {
-		t.Error(err)
-	}
-
-	nresults := len(resp.Data)
-	for i, zone := range resp.Data {
-		parts := strings.Split(zone, "/")
-		t.Logf("(%d/%d) %q", i+1, nresults, parts[len(parts)-2])
-	}
-}
-
-func TestFetchingAllZoneRecords(t *testing.T) {
-	client := NewClient(DynCustomerName)
-	client.Verbose(true)
-	err := client.Login(DynUsername, DynPassword)
-	if err != nil {
-		t.Error(err)
-	}
-	defer func() {
-		err = client.Logout()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-
-	var resp AllRecordsResponse
-	err = client.Do("GET", "AllRecord/"+testZone, nil, &resp)
-	if err != nil {
-		t.Error(err)
-	}
-	for _, zr := range resp.Data {
-		parts := strings.Split(zr, "/")
-		uri := strings.Join(parts[2:], "/")
-		t.Log(uri)
-
-		var record RecordResponse
-		err := client.Do("GET", uri, nil, &record)
-		if err != nil {
+		if err := c.Do("GET", "Zone", nil, &resp); err != nil {
 			t.Fatal(err)
 		}
 
-		t.Log("OK")
-	}
+		nresults := len(resp.Data)
+		for i, zone := range resp.Data {
+			parts := strings.Split(zone, "/")
+			t.Logf("(%d/%d) %q", i+1, nresults, parts[len(parts)-2])
+		}
+	})
+}
+
+func TestFetchingAllZoneRecords(t *testing.T) {
+	testWithClientSession("fixtures/fetching_all_zone_records", t, func(c *Client) {
+		var resp AllRecordsResponse
+
+		if err := c.Do("GET", "AllRecord/"+testZone, nil, &resp); err != nil {
+			t.Error(err)
+		}
+
+		for _, zr := range resp.Data {
+			parts := strings.Split(zr, "/")
+			uri := strings.Join(parts[2:], "/")
+			t.Log(uri)
+
+			var record RecordResponse
+
+			if err := c.Do("GET", uri, nil, &record); err != nil {
+				t.Fatal(err)
+			}
+
+			t.Log("OK")
+		}
+	})
 }
